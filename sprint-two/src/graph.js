b690798fe7b2e5f6039ae7448004c7b0045772ff
fasterthansky <fasterthansky@gmail.com>
2020-02-10 00:21:24 -0800

    submit pair work, updated hash tables from lecture.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,35 +2,90 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = new Node(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var key in this.storage) {
+    if (this.storage[key].value === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+
+  var keys = [];
+
+  for (var key in this.storage[node]) {
+    if (key !== 'value'){
+      keys.push(key)
+    }
+  }
+  for (var i = 0; i < keys.length; i++) {
+
+    console.log(this.storage[keys[i]]);
+    delete this.storage[keys[i]][node];
+  }
+
+
+  delete this.storage[node];
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-};
+  // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+  Graph.prototype.hasEdge = function(fromNode, toNode) {
+    if (this.storage[fromNode][toNode]) {
+      return true;
+    } else {
+      return false;
+    }
 
-// Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-};
+  };
 
-// Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
-};
+  // Connects two nodes in a graph by adding an edge between them.
+  Graph.prototype.addEdge = function(fromNode, toNode) {
+    // this.storage[toNode][fromNode] = this.storage[toNode];
+    // this.storage[toNode][fromNode] = this.storage[fromNode];
+    // debugger;
+    // this.storage[fromNode][toNode] = this.storage[fromNode];
+    // this.storage[toNode];
 
-// Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
-};
+    this.storage[toNode][fromNode] = this.storage[fromNode];
+    this.storage[fromNode][toNode] = this.storage[toNode];
+  };
+
+  // Remove an edge between any two specified (by value) nodes.
+  Graph.prototype.removeEdge = function(fromNode, toNode) {
+    delete this.storage[toNode][fromNode];
+    delete this.storage[fromNode][toNode];
+
+  };
+
+  // Pass in a callback which will be executed on each node of the graph.
+  Graph.prototype.forEachNode = function(cb) {
+    for (var key in this.storage) {
+      cb(key);
+    }
+  };
+
+  /*
+   * Complexity: What is the time complexity of the above functions?
+   */
+
+
+  var Node = function(value) {
+    var node = {};
+    this.value = value;
+  };
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,25 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    // var newHead = list.head.next;
+    var result = list.head.value;
+    // delete list.head;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target) {
-  };
+    var node = list.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+
+      node = node.next;
+    }
+
+    return false;
+      };
 
   return list;
 };
 
-var Node = function(value) {
-  var node = {};
-
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
 
 /*
  * Complexity: What is the time complexity of the above functions?
